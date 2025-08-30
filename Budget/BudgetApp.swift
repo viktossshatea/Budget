5ee8d8842c9804b3f5d3a97075af33904bc38170
Viktosssha Tea <petrosyanvikusha@gmail.com>
2025-08-30 22:15:49 +0300

    Initial Commit

---
+++ Budget/BudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetApp.swift
+//  Budget
+//
+//  Created by  Виктория on 30.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct BudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
