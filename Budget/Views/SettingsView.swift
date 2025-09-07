e1de412bc2bf09a1a4e970ef255de9b51d62c077
Viktosssha Tea <petrosyanvikusha@gmail.com>
2025-09-07 16:36:57 +0300

    First Commit

---
+++ Budget/Views/SettingsView.swift
@@ -0,0 +1,18 @@
+//
+//  SettingsView.swift
+//  Budget
+//
+//  Created by  Виктория on 06.09.2025.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
